6798585668edd60eb2c311f2923dd9094709125f
deepika30gowda-bca <deepika30gowda@gmail.com>
2026-06-08 17:31:53 +0530

    Create Sum_of_digits.sh

---
+++ Sum_of_digits.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+read -p " Enter a number :" num
+sum = 0
+while (( num>0 )); do
+    digit = $((num %. 10))
+    sum = $((sum + digit))
+    num = $((num /10))
+done
+echo " The sum of digits is : $sum"
